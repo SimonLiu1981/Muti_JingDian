2e742bc321d492976546fc1224e27bcc2b6ea5c5
SimonLiu1981 <sgci@163.com>
2014-01-20 23:32:06 -0800

    add HtmlHelpers.cs

---
+++ src/dx177.FrameWork/HtmlHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+
+namespace dx177.FrameWork
+{
+    public static class HtmlHelpers
+    {
+
+        public static string Truncate(string input, int length)
+        {
+            if (input.Length <= length)
+            {
+                return input;
+            }
+            else
+            {
+                return input.Substring(0, length) + "...";
+            }
+        }
+
+        public static string RevmoeHtmlTag(string input)
+        {
+            return Regex.Replace(input, "<.*?>", string.Empty);  
+        }
+    }
+}
